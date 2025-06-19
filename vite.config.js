0c8b71a69a46ad77ac843b52eaecb49ac6dcfc50
parthmital <parth.mital.2004@gmail.com>
2025-06-19 21:37:10 +0530

    Rename

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite'
 import preact from '@preact/preset-vite'
 
 export default defineConfig({
-  base: '/portfolio-preact/',
+  base: '/Portfolio/',
   plugins: [preact()],
 })
